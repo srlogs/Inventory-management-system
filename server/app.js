73372f575a1413a29a63d49fa21c57275c874a34
srlogs <logeshwaran.sr1@gmail.com>
2021-01-04 13:43:53 +0530

    project intiation

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log("server is up...");
+})

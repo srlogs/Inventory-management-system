7a5201b1b81d8bc1253e78a835426ff2009ee092
srlogs <logeshwaran.sr1@gmail.com>
2021-01-05 11:07:48 +0530

    removed modules

--- server/middleware/authJwt.js
+++ server/middleware/authJwt.js
@@ -5,10 +5,16 @@ validate = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) {
-        return token;
+        return res.status(403).send({
+            message: "No token provided!"
+        });
     }
     jwt.verify(token, process.env.SECRET_TOKEN, (err, data) => {
-        if (err) throw err;
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!"
+            });
+        }
         req.user = data.emailid;
         next();
     })
